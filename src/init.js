d0fbbebfb618e4ca604657cc343aeddada1e60b1
Carlos Muñoz <carloxmunozr.12@gmail.com>
2020-05-31 17:06:32 -0500

    anims with atlas

--- src/init.js
+++ src/init.js
@@ -3,9 +3,52 @@ const config = {
     height: 600,
     parent: "container",
     type: Phaser.AUTO,
-    backgroundColor: "#392542",
-    scene: []
+    pixelArt: true,
+    backgroundColor: "#222",
+    scene: {
+        preload,
+        create
+    }
 }
 
 new Phaser.Game(config)
 
+
+function preload() {
+    // this.load.spritesheet('evil_tomato', './assets/evil_tomato.png', { frameWidth: 16, frameHeight: 25})
+    this.load.atlas('tomato_atlas', './assets/evil_tomato.png', './assets/evil_tomato.json')
+    this.load.json('evil_tomato_anim', './assets/evil_tomato_anim.json')
+}
+
+function create() {
+    this.tomato = this.add.sprite(125, 150,'tomato_atlas').setScale(4)
+    this.dataAnim = this.cache.json.get('evil_tomato_anim')
+
+    // this.anims.create({
+    //     key: 'tomato_walk',
+    //     frames: this.anims.generateFrameNumbers('evil_tomato', {
+    //         //frames: [1, 2, 3, 4, 5, 6, 7, 8]
+    //         start: 1,
+    //         end: 8
+    //     }),
+    //     repeat: -1,
+    //     frameRate: 10
+    // })
+
+    // this.anims.create({
+    //     key: 'tomato_walk',
+    //     frames: this.anims.generateFrameNames('evil_tomato', {
+    //         //frames: [1, 2, 3, 4, 5, 6, 7, 8]
+    //         prefix: 'evil_tomato_',
+    //         suffix: '.png',
+    //         start: 1,
+    //         end: 8
+    //     }),
+    //     repeat: -1,
+    //     frameRate: 10
+    // })
+
+    this.anims.fromJSON(this.dataAnim)
+
+    this.tomato.anims.play('tomato_atlas_walk')
+}
