c2796f1b576008cfbfe6669f844933d4e72ab3b1
Carlos Muñoz <carloxmunozr.12@gmail.com>
2020-05-31 16:15:24 -0500

    reset project animation

--- src/init.js
+++ src/init.js
@@ -1,13 +1,10 @@
-import SceneA from './sceneA.js'
-import SceneB from './sceneB.js'
-
 const config = {
     width: 800,
     height: 600,
     parent: "container",
     type: Phaser.AUTO,
     backgroundColor: "#392542",
-    scene: [SceneA, SceneB]
+    scene: []
 }
 
 new Phaser.Game(config)
