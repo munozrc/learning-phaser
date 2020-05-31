5e6c2f99c1a14693442fc798cf3e9857fdae7b97
Carlos Muñoz <carloxmunozr.12@gmail.com>
2020-05-30 19:51:35 -0500

    carga de imagen y fisica

--- src/init.js
+++ src/init.js
@@ -1,25 +1,41 @@
 const config = {
-    width: 320,
-    height: 180,
+    width: 320*2,
+    height: 180*2,
     parent: "container",
     type: Phaser.AUTO,
     scene: {
         preload: preload,
         create: create,
         update: update
+    },
+    physics: {
+        default: "arcade",
+        arcade: {
+            gravity: {
+                y: 500
+            }
+        }
     }
 }
 
 var game = new Phaser.Game(config)
 
 function preload() {
-    console.log("init preload")
+    this.load.image("bird", "./assets/bird.png")
 }
 
 function create() {
-    console.log("init create")
+    this.bird = this.physics.add.image(50, 100, "bird")
+    this.bird.setScale(2)
+    this.bird.flipX = false
+    this.bird.setOrigin(0.5)
+    // Physics
+    this.bird.setCollideWorldBounds(true)
+    this.bird.setBounce(0.3)
+    //this.bird.setVelocity(50, 0)
+    this.bird.setAcceleration(50,0)
 }
 
 function update() {
-    
+
 }
