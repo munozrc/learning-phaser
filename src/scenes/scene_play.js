df5686e922dc9dda8caf1e1528305272f02d3eef
Carlos Muñoz <carloxmunozr.12@gmail.com>
2020-05-31 15:42:48 -0500

    tres

--- src/scenes/scene_play.js
+++ src/scenes/scene_play.js
@@ -1,3 +1,5 @@
+import Pallete from '../gameObjects/pallete.js'
+
 class Scene_play extends Phaser.Scene {
 
     constructor() {
@@ -11,12 +13,27 @@ class Scene_play extends Phaser.Scene {
 
         this.add.image(center_width, center_height,"separator")
 
-        this.left = this.add.image(30, center_height, "left")
-        this.right = this.add.image(this.sys.game.config.width - 30 , center_height, "right")
-    
-        this.ball = this.physics.add.image(center_width, center_height, "ball")
+        // Set Palletes
+        // this.left = this.add.image(30, center_height, "left")
+        this.left = new Pallete(this, 30, center_height, "left")
 
-        // this.ball.setVelocityX(-180)
+        // this.right = this.add.image(this.sys.game.config.width - 30 , center_height, "right")
+        this.right = new Pallete(this, this.sys.game.config.width - 30, center_height, "right")
+
+        // Set Ball
+        this.physics.world.setBoundsCollision(false, false, true, true)
+        this.ball = this.physics.add.image(center_width, center_height, "ball")
+        this.ball.setCollideWorldBounds(true)
+        this.ball.setBounce(1)
+        this.ball.setVelocityX(-180)
+
+        // Set Physics
+        this.physics.add.collider(this.ball, this.left, this.hitPallete, null, this)
+        this.physics.add.collider(this.ball, this.right, this.hitPallete, null, this)
+    }
+
+    hitPallete() {
+
     }
 
 }
