2c04dfc6378871a6fed339c714414817b9f58d2a
Carlos Muñoz <carloxmunozr.12@gmail.com>
2020-05-30 21:44:20 -0500

    escenas

--- src/init.js
+++ src/init.js
@@ -1,54 +1,11 @@
 const config = {
-    width: 320*2,
-    height: 180*2,
+    width: 800,
+    height: 600,
     parent: "container",
     type: Phaser.AUTO,
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
-    },
-    physics: {
-        default: "arcade",
-        arcade: {
-            gravity: {
-                //y: 500
-            }
-        }
-    }
+    backgroundColor: "#392542",
+    scene: [SceneA, SceneB]
 }
 
-var game = new Phaser.Game(config)
+new Phaser.Game(config)
 
-function preload() {
-    this.load.image("bird", "./assets/bird.png")
-}
-
-function create() {
-    this.bird = this.physics.add.image(100, 50, "bird")
-
-    // this.input.keyboard.on("keydown_RIGHT", () => {
-    //     this.bird.setAcceleration(100, 0)
-    // })
-
-    // this.input.keyboard.on("keyup_RIGHT", () => {
-    //     this.bird.setAcceleration(0, 0)
-    //     this.bird.setVelocity(0)
-    // })
-
-    //this.cursor = this.input.keyboard.createCursorKeys()
-
-    this.right = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D)
-    this.left = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A)
-}
-
-function update() {
-    //this.cursor.right.isDown
-    if (this.right.isDown) {
-        this.bird.x++
-        this.bird.flipX = false
-    } else if (this.left.isDown) {
-        this.bird.x--
-        this.bird.flipX = true
-    }
-}
---
+++ src/sceneA.js
@@ -0,0 +1,30 @@
+class SceneA extends Phaser.Scene {
+
+    constructor() {
+        super({
+            key: "SceneA"
+        })
+    }
+
+    preload() {
+
+    }
+
+    create() {
+        let graphics = this.add.graphics()
+
+        graphics.fillStyle(0xff3300, 1)
+
+        graphics.fillRect(100, 200, 600, 300)
+        graphics.fillRect(100, 100, 100, 100)
+
+        this.add.text(120, 105, "A", { font: "96px Courier", fill: "#000000"})
+    
+        this.scene.add("SceneC", new SceneC)
+        this.scene.start("SceneC")
+    }
+
+    update(time, delta) {
+
+    }
+}
